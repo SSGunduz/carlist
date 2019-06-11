e192eda010cc3b958f2caa17991062ae7d62b0d7
Suat Guenduez, Serdar (EXTERN: Hexad) <serdar.suat@hexad.de>
2019-06-11 14:32:55 +0200

    Resources and application properties

---
+++ src/main/java/com/carlisting/carlist/CarlistApplication.java
@@ -0,0 +1,12 @@
+package com.carlisting.carlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarlistApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/carlisting/carlist/constants/UrlConstants.java
@@ -0,0 +1,10 @@
+package com.carlisting.carlist.constants;
+
+public class UrlConstants {
+
+    private UrlConstants() {
+    }
+
+    public static final String URL_MAPPING_API_SEARCH = "/carlist/api/v1/cars";
+    public static final String URL_MAPPING_API_UPLOAD = "/carlist/api/v1/cars/files";
+}
